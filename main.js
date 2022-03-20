83fbd28eda6d4ba8c0eff94afce947315e9da378
Bhavishyaa Vignesh <67924047+bhavicodey@users.noreply.github.com>
2022-03-20 14:45:43 -0700

    Add files via upload

--- main.js
+++ main.js
@@ -23,9 +23,19 @@ var ball = {
 
 function setup(){
   var canvas =  createCanvas(700,600);
+  canvas.parent('canvas');
+
+  video = createCanvas(VIDEO);
+  video.size(700, 600);
+  video.hide();
+
+  poseNet = ml5.poseNet(video, modelLoaded);
+  poseNet.on('pose', gotPoses);
 }
 
-
+function modelLoaded() {
+  console.log("Model Loaded!")
+}
 function draw(){
 
  background(0); 
